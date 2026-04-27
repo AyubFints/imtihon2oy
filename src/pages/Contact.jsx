1b2ab6f175c4ba25cb981260f2f9943e7afe2ca1
Ayubxon Xamidov <ayyuubxonxamidov@gmail.com>
2026-04-27 09:39:53 +0500

    yangianish

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,28 @@
-import "./style.css"
+import "./style.css";
 
-function Contact(){
-    return(
-        <div>
-            
-        </div>
-    )
+function Contact() {
+  return (
+    <div>
+
+      <div className="contact">
+        <h1 style={{marginTop:150}}>Contact us</h1>
+        <p>
+          Discover why homeowners trust ICal Construction. Our clients’ experiences speak for themselves—
+          <br />
+          see their testimonials as they share their journey from concept to completion.
+        </p>
+      </div>
+
+      <div className="map-container">
+        <iframe
+          title="uzbekistan-map"
+          src="https://www.google.com/maps?q=Uzbekistan&output=embed"
+          loading="lazy"
+        ></iframe>
+      </div>
+
+    </div>
+  );
 }
+
 export default Contact;
